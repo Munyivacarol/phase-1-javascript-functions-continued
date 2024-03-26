658eb0b05acf1d15b5d1a07c3d35e029834b1701
Carolyne munyiva <munyivacarolyne571@gmail.com>
2024-03-26 10:40:51 +0300

    functions done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+//declare the second function here
+
+function mondayWork(place = 'go to the office'){
+    return `This Monday, I will ${place}.`;
+}
+//declaring the wrap adjective
+function wrapAdjective(result) {
+
+    return function(par = "special") {
+        return `You are ${result}${par}${result}!`
+    };
+    }
